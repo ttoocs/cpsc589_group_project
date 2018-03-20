7d89dc7b4de8c34ef8f66c9864328051c2f69617
ttoocs <ttoocs@gmail.com>
2018-03-19 20:54:01 -0600

    Moved some stuff from main into a header.

---
+++ prog/main.h
@@ -0,0 +1,38 @@
+#pragma once
+
+//main.h
+
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <sstream>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//#include <Eigen/Dense>
+
+
+#include "types.h"
+#include "gl_helpers.h"
+#include "shapes.h"
+#include "input.h"
+
+
+
+//#define WIREFRAME
+#define DEBUG
+
+#ifdef DEBUG
+	#define DEBUGMSG	printf("\n\n\t\t DEBUG MESSAGE AT LINE:\t%d\t In file:\t%s\n\n",__LINE__,__FILE__);
+#else 
+	#define DEBUGMSG	;
+#endif
+
+
+#define torad(X)	((float)(X*PI/180.f))
