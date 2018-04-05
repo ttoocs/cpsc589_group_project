0c86d171c18961bb0ebc9a974174a0614f09296a
ttoocs <ttoocs@gmail.com>
2018-04-04 21:58:04 -0600

    Cloud mods, less cube, more.. blob.

--- prog/cloud/cloud.cpp
+++ prog/cloud/cloud.cpp
@@ -1,10 +1,18 @@
 //Scott Saunders
 // 1: The most basic of basic cloud, a meta-ball.
+// 3: Place them randomly in the 5x5 box
+// 4: Remove 5x5 box-thing, make it randomly select X
+// 5: Make it's granularity 0.25, -> faster (lower res march).
 
+//Cory Jenson
+// 2: Create_cloud
 #include "cloud.h"
 #include "stdlib.h"
 #include <vector>
 
+#include "../main.h"
+
+int AvgNew = 10;
 
 using namespace std;
 void cloud::create_cloud(vector<vec3> *points, vector<GLuint> *indices,vector<vec3> *norms, int rounds)
@@ -17,17 +25,18 @@ void cloud::create_cloud(vector<vec3> *points, vector<GLuint> *indices,vector<ve
 	float r1 = 0.0f;
 	float radius = ((float)rand())/((float)INT_MAX);
 	
-	//balls;
-    points->clear();
-    indices->clear();
-    norms->clear();
-	MetaBall::March(points,indices,norms, &balls);
 	int a=0;
 	for(int j = 0; j < rounds; j++)
 	{
+    points->clear();
+    indices->clear();
+    norms->clear();
+  //March getting points/indexs/norms, on balls, unknown bounding boxes, granularity =0.25 (half-res of new default)
+	  MetaBall::March(points,indices,norms, &balls, NULL, NULL, 0.25);
 		for(int i = 0; i < indices->size()/3; i++)
 		{
-      if(((float)rand())/((float)INT_MAX) > 0)
+      float rng = (((float)rand())/((float)INT_MAX));
+      if(rng < ((float) AvgNew)/((float)(indices->size()/3)))
       {
         //START: Random position inside square for new metaball
         p0 = (*points)[(*indices)[i*3]];
@@ -41,15 +50,18 @@ void cloud::create_cloud(vector<vec3> *points, vector<GLuint> *indices,vector<ve
         p0 = (1.0f-r1-r0)*p2 + r1*p1 + r0*p0;
         
         //END: Random position inside square for new metaball
+        //Random size:
+        float rad = 1*((float)rand())/((float)INT_MAX);
         
-        balls.push_back((new MetaBall(p0+vec3(rand()%5,rand()%5,rand()%5),0.1*((float)rand())/((float)INT_MAX),balls[0]->m_surfaceFunction)));
+        balls.push_back((new MetaBall(p0,rad,balls[0]->m_surfaceFunction)));
         std::cout << a++ << std::endl;
       }
 		}
-    points->clear();
-    indices->clear();
-    norms->clear();
-		MetaBall::March(points,indices,norms, &balls);
+    std::cout << "cloud round done" << std::endl;
 	}
+  points->clear();
+  indices->clear();
+  norms->clear();
+	MetaBall::March(points,indices,norms, &balls, NULL, NULL, 0.25);
   cout <<"Done making a cloud\n";
 }
