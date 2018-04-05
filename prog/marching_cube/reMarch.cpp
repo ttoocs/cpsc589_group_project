e1d8dccfc866de105f4f1fc2a9ed554bedcdea1e
ttoocs <ttoocs@gmail.com>
2018-04-04 20:19:31 -0600

    Added and enabled openMP on marching cubes, ~2.5 sec on metaBalltest -> .677 sec.

--- prog/marching_cube/reMarch.cpp
+++ prog/marching_cube/reMarch.cpp
@@ -19,15 +19,38 @@ void March1(double (*f)(double, double, double),
     GLint tcount;
     GLfloat vertices[5*3*3];
 
+#define USEMP
+#ifdef USEMP
+    //OpenMP: Doesn't like floats
+    // To fix this, we can scale everything to stepsize of '1'
+    double scale = 1/a_granularity;
+    
+        #pragma omp parallel for
+    for(int ix = (int) (a_lowerBound.x*scale); ix <= (int) (a_upperBound.x*scale + 1); ix++){
+        #pragma omp parallel for
+      for(int iy = (int) (a_lowerBound.y*scale); iy <= (int)(a_upperBound.y*scale + 1); iy++){
+        #pragma omp parallel for private(vertices, tcount)
+        for(int iz = (int)(a_lowerBound.z*scale); iz <= (int)(a_upperBound.z*scale + 1); iz++){
+          
+          double x = ((double) ix)*a_granularity;
+          double y = ((double) iy)*a_granularity;
+          double z = ((double) iz)*a_granularity;
+//          std::cout << "(" << ix << "," << iy << "," << iz << ")\t(" << x << "," << y << "," << z << ")" << std::endl;
+
+#else
     // sample the implicit surface by stepping through each dimension
-    for (GLfloat x = a_lowerBound.x; x <= a_upperBound.x; x += a_granularity)
-        for (GLfloat y = a_lowerBound.y; y <= a_upperBound.y; y += a_granularity)
-            for (GLfloat z = a_lowerBound.z; z <= a_upperBound.z; z += a_granularity)
-            {
+    for (float x = a_lowerBound.x; x <= a_upperBound.x; x += a_granularity){
+        for (float y = a_lowerBound.y; y <= a_upperBound.y; y += a_granularity){
+            for (float z = a_lowerBound.z; z <= a_upperBound.z; z += a_granularity){
+            
+
+#endif
                 // call marching cubes to get the triangular facets for this cell
                 vMarchCubeCustom(x, y, z, a_granularity, f, tcount, vertices);
 
                 // add resulting triangles (if any) to our mesh
+                  //Std::vectors are _not_ multithread safe.
+                #pragma omp critical
                 for (int i = 0; i < tcount; ++i) {
                     int ix = i*9;
                     verts->push_back(vec3(vertices[ix+0], vertices[ix+1], vertices[ix+2]));
@@ -46,5 +69,8 @@ void March1(double (*f)(double, double, double),
                       norms->push_back(   getNorm(f , (*verts)[verts->size()-1] ) );
                 }
             }
+        }
+    }
+//    std::exit(0);
 }
 
